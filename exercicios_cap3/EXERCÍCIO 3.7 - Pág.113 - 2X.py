85098b1325bed184737d13156b44f093524a0f36
marioruiromao <marioruiromao@outlook.com>
2026-07-04 13:13:38 +0100

    Adicionar exercícios do capítulo 5 do Eric Matthes

--- exercicios_cap3/EXERCÍCIO 3.7 - Pág.113 - 2X.py
+++
@@ -1,4 +0,0 @@
-numero = int(input("Digite um número:"))
-numero2 = int(input("Digite um outro número:"))
-soma = print(numero + numero2)
-
--- exercicios_cap3/EXERCÍCIO 3.7 - Pág.113.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input("Introduza um nº inteiro: " ))
-n2 = int(input("Introduza outro nº inteiro: "))
-soma = n1 + n2
-print(soma)
-
